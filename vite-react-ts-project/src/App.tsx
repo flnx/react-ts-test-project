e51d823df315f09dcf37b34ae0629582977a7c28
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-08-22 11:09:10 +0300

    Adding a list

--- vite-react-ts-project/src/App.tsx
+++ vite-react-ts-project/src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+
+// Components
 import Counter from './components/Counter';
 import Heading from './components/Heading';
 import Section from './components/Section';
+import List from './components/List';
 
 function App() {
     const [count, setCount] = useState<number>(1);
@@ -11,6 +14,10 @@ function App() {
             <Heading title={'Hello There!'} />
             <Section title={'Different title'}>This is a Section</Section>
             <Counter setCount={setCount}>Count is {count}</Counter>
+            <List
+                items={['☕ Coffee', '🌮 Tacos', '💻 Code']}
+                render={(item: string) => <span className="gold">{item}</span>}
+            />
         </>
     );
 }
