c252ccf665bfb2b29b132bc6fbb7fd954d03604b
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-08-22 10:25:48 +0300

    Adding Counter

--- vite-react-ts-project/src/App.tsx
+++ vite-react-ts-project/src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
+import Counter from './components/Counter';
 import Heading from './components/Heading';
 import Section from './components/Section';
 
 function App() {
+    const [count, setCount] = useState<number>(1);
+
     return (
         <>
             <Heading title={'Hello There!'} />
             <Section title={'Different title'}>This is a Section</Section>
+            <Counter setCount={setCount}>Count is {count}</Counter>
         </>
     );
 }
